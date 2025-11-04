9d537a4661637437e7525689cf394fa540f6ffb7
Simon1812 <simonklierl@web.de>
2025-11-04 22:49:49 +0100

    add graphical visualization and make sleep_duration configurable

--- play.py
+++ play.py
@@ -4,8 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-
-
 class Game:
     def __init__(self, config):
         grid_size = config["grid_size"]
@@ -19,6 +17,7 @@ class Game:
         self.dead_char = " ‧ " # config["dead_char"]
         self.max_steps = config["max_steps"]
         self.visualization_mode = config["visualization"]["mode"]
+        self.sleep_duration = config["sleep_duration"]
 
     def init_grid(self):
         tmp = np.zeros((self.grid_size[0]+2, self.grid_size[1]+2), dtype=int)
@@ -35,13 +34,15 @@ class Game:
             self.visualize(grid[1:-1, 1:-1])
             time.sleep(0.5)
 
+        plt.show()
+
     def visualize(self, grid):
         if self.visualization_mode == "text":
             for row in grid:
                 print("".join([self.alive_char if cell else self.dead_char for cell in row]))
         elif self.visualization_mode == "graphical":
             plt.imshow(grid, cmap="binary")
-            plt.show()
+            plt.pause(0.05)
         else:
             raise ValueError("Unknown visualization mode")
         
