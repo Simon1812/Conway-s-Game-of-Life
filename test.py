e7b50e9f4742b7019b807e1f83958216b8230383
Simon1812 <103826918+Simon1812@users.noreply.github.com>
2025-11-05 14:28:17 +0100

    update test for alive based update mode

--- test.py
+++ test.py
@@ -20,6 +20,7 @@ class testgame(unittest.TestCase):
     
     def test_update_grid1(self):
         # Test case create block pattern
+        grid =np.zeros((7,7))
         initial_grid = np.array([[0, 0, 0, 0, 0],
                                   [0, 1, 1, 0, 0],
                                   [0, 0, 1, 0, 0],
@@ -31,8 +32,9 @@ class testgame(unittest.TestCase):
                                   [0, 1, 1, 0, 0],
                                   [0, 0, 0, 0, 0],
                                   [0, 0, 0, 0, 0]])
-        updated_grid = self.game.update_grid(initial_grid)
-        np.testing.assert_array_equal(updated_grid, expected_grid)
+        grid[1:-1, 1:-1] = initial_grid
+        updated_grid = self.game.update_grid(grid)
+        np.testing.assert_array_equal(updated_grid[1:-1, 1:-1], expected_grid)
     
     def test_update_grid2(self):
         # Test case with death by underpopulation, death by overpopulation, and reproduction
